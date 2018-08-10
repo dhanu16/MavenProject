25a441bcdd43f21d69c5154eddd617e311524b93
User1 <User1@Z1R3P2>
2018-08-10 13:20:15 +0530

    second class

---
+++ MavenProject/src/com/maven/POMworking/SecondClass.java
@@ -0,0 +1,14 @@
+package com.maven.POMworking;
+
+public class SecondClass {
+
+	public void xmlCheck1(){
+		System.out.println("check working of testng.xml");
+	}
+	public void xmlCheck2(){
+		System.out.println("check working of testng.xml");
+	}
+	public void xmlCheck3(){
+		System.out.println("check working of testng.xml");
+	}
+}
