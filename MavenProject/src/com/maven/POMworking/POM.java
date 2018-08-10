26bad3cc3d52103668c55a54bbc5f178c082b144
User1 <User1@Z1R3P2>
2018-08-10 12:08:26 +0530

    testng practise

--- MavenProject/src/com/maven/POMworking/POM.java
+++ MavenProject/src/com/maven/POMworking/POM.java
@@ -1,5 +1,19 @@
 package com.maven.POMworking;
 
+import org.testng.annotations.Test;
+
 public class POM {
 
+@Test
+public void Test1(){
+	System.out.println("done!!!");
+}
+@Test
+public void Test2(){
+	System.out.println("done!!!");
+}
+@Test
+public void Test3(){
+	System.out.println("done!!!");
+}
 }
